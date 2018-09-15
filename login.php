a185a1c2d663d8de827a67391bfb806b6cfa86ce
lawchellie <lawchellie@yahoo.com>
2018-09-15 06:45:13 +0100

    My first publish
    Testing this on github

---
+++ login.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html >
+  <head>
+    <meta charset="UTF-8">
+	<title>Marina Boyblos Hotel</title>
+	
+	<link rel="icon" href="logo.png" type="image" />
+	<link rel="shortcut icon" href="logo.png" type="image" />
+    
+    
+    
+    <!--<link rel='stylesheet prefetch' href='http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css'>-->
+
+        <link rel="stylesheet" href="css/style.css">
+
+    
+    
+    
+  </head>
+
+  <body>
+
+    <div class="login-form">
+     <div id="h1">
+		<a href="index.html" title="Click to go to home"><img src="images/logo.png" width="107" height="66" alt="" /></a>
+	</div>
+	<br>
+     <div class="form-group ">
+       <input type="text" class="form-control" placeholder="Username " id="UserName">
+       <i class="fa fa-user"></i>
+     </div>
+     <div class="form-group log-status">
+       <input type="password" class="form-control" placeholder="Password" id="Passwod">
+       <i class="fa fa-lock"></i>
+     </div>
+      <span class="alert">Invalid Credentials</span>
+      <!--<a class="link" href="#">Lost your password?</a>--><br>
+     <button type="button" class="log-btn" >Log in</button>
+     
+    
+   </div>
+    <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+
+        <script src="js/index.js"></script>
+
+    
+    
+    
+  </body>
+</html>
